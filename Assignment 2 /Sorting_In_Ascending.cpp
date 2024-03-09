201421f46845fdb82b48bd042d3da2770fd2980c
Sami Ullah <157210731+SamiiiU@users.noreply.github.com>
2024-03-10 07:59:29 +0930

    Sorting_In_Ascending.cpp

---
+++ Assignment 2 /Sorting_In_Ascending.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<array>
+
+/* Write a program that arrange the numbers enter by user in ascending order*/
+int main(){
+    //input size
+    int n;
+    std::cout << "Enter the size of array : ";
+    std::cin >> n;
+    
+    int arr[n];
+    std::cout<<"Enter Numbers To Fill Out The Array"<<std::endl;
+    //input of array
+    for(int i = 0 ; i < n; i++ ){
+        std::cin>>arr[i];
+    }
+    
+    //checking array
+    for(int  i= 0 ; i < n - 1 ; i++){
+        for(int j = 1+i ; j < n ; j++){
+            if(arr[i] > arr[j]){
+                arr[i] = arr[i] + arr[j];
+                arr[j] = arr[i] - arr[j];
+                arr[i] = arr[i] - arr[j];
+            }
+        }
+    }
+    //printing array in ascending order
+    for(int i = 0 ; i < n ; i++){
+        std::cout << arr[i] << "\t";
+    }
+    return 0;
+}
